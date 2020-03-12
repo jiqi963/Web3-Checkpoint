3fe9a291fc00ab4c4efd3a71682a5794d4e95afc
Ke Wang (1000048722) <WANGK6@student.op.ac.nz>
2020-03-13 11:47:41 +1300

    ck 5

--- templates/app.py
+++ templates/app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_templates
+from mongoengine import *
+connect('web3')
+
 
 app = Flask(__name__)
 
@@ -6,7 +9,17 @@ app = Flask(__name__)
 @app.route('/index')
 @app.route('/home')
 def index():
-    return render_templates('index.html')
-    
+    index= "Index"
+    return render_templates('index.html', title=index)
+
+
+@app.route('/1.1')
+def  1.1():
+    return render_templates('1.1.html')    
+
+@app.route('/1.2')
+def 1.2():
+    return render_templates('1.2.html')	
+	
 if __name__ == '__main__':
     app.run(debug=True)
