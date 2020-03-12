fcee2d9333a9f4b77b2ef55c2f4d3e098d901ee3
Ke Wang (1000048722) <wangk6@student.op.ac.nz>
2020-03-12 23:18:05 +1300

    doing ck5

---
+++ templates/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_templates
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+@app.route('/home')
+def index():
+    return render_templates('index.html')
+    
+if __name__ == '__main__':
+    app.run(debug=True)
