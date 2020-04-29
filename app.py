3d921b15c81aa06b020818b9f028f2ce8f9a417a
kevin <jiqi963@gmail.com>
2020-04-29 14:25:54 +1200

    update

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, url_for
 from mongoengine import *
+import os
+import csv
 
-app.config.from_object('config')
 
 connect('web3')
 
@@ -10,21 +11,17 @@ class Country(Document):
     Continent = StringField()
     CountryCode = StringField()
 
+
 app = Flask(__name__)
+
+
+app.config.from_object('config')
 
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
 def index():
     index= "Index"
-    for file in op.listdir(app.config['FILES_FOLDER']):
-        filename = op.fsdecode(file)
-        path = os.path.join(app.config['FILES_FOLDER'],"data1.csv")
-        f = open(path)
-        r = csv.reader(f)
-        d = list(r)
-        for data in d
-            print(data)
     return render_template('index.html', title=index)
 
 @app.route('/pageone')
@@ -42,7 +39,6 @@ def create():
 	China.save(force_insert=False)
 	NewZealand = Country(CountryID='2',Continent='Oceania',CountryCode='NZ' )
 	NewZealand.save(force_insert=True)
-
 	return render_template('index.html')
 
 @app.route('/countries', methods=['GET','POST','PUT'])
@@ -54,6 +50,19 @@ def getCountry(CountryCode=None):
 	else:
 		countries = Country.objects.get(CountryCode=CountryCode)
 	return countries.to_json()
+
+@app.route('/data')
+def data():
+    for file in os.listdir(app.config['FILES_FOLDER']):
+        filename = os.fsdecode(file)
+        path = os.path.join(app.config['FILES_FOLDER'],"data1.csv")
+        f = open(path)
+        r = csv.reader(f)
+        d = list(r)
+        for data in d:
+            print(data)
+    return data.to_json()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True, port=80)
